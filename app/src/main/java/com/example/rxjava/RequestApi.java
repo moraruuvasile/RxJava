364041617ef0796619df4768b497722c3078bc5b
Vasile <vaseoa@yahoo.com>
2020-07-05 20:59:34 +0300

    first

---
+++ app/src/main/java/com/example/rxjava/RequestApi.java
@@ -0,0 +1,17 @@
+package com.example.rxjava;
+
+import java.util.List;
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+interface RequestApi {
+    @GET("posts")
+    Observable<List<Post>> getPosts();
+
+    @GET("posts/{id}/comments")
+    Observable<List<Comment>> getComments(
+            @Path("id") int id
+    );
+}
