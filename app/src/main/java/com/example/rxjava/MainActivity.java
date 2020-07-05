0ae14888305ac3fb81b4ce643bd0db5e96a78470
Vasile <vaseoa@yahoo.com>
2020-07-05 18:33:39 +0300

    first

--- app/src/main/java/com/example/rxjava/MainActivity.java
+++ app/src/main/java/com/example/rxjava/MainActivity.java
@@ -8,50 +8,54 @@ import androidx.appcompat.app.AppCompatActivity;
 import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
-    CompositeDisposable disposables = new CompositeDisposable();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Observable<Task> taskObservable = Observable // create a new Observable object
-                .fromIterable(Task.createTasksList()) // apply 'fromIterable' operator
-                .subscribeOn(Schedulers.io()) // designate worker thread (background)
-                .observeOn(AndroidSchedulers.mainThread()); // designate observer thread (main thread)
+        Observable.fromIterable(Task.createTasksList())
+                .map(function)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(String task) {
+                        Log.d("vasea", "onNext: " + task);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
 
 
-
-        taskObservable.subscribe(new Observer<Task>() {
-            @Override
-            public void onSubscribe(Disposable d) {
-                disposables.add(d);
-            }
-            @Override
-            public void onNext(Task task) { // run on main thread
-
-                Log.d("vasea", "onNext: : " + task.getDescription());
-            }
-            @Override
-            public void onError(Throwable e) {
-
-            }
-            @Override
-            public void onComplete() {
-                Log.d("vasea", "onComplete: : ");
-            }
-        });
     }
 
+    Function<Task, String> function = new Function<Task, String>() {
+        @Override
+        public String apply(Task task) {
+            return task.toString();
+        }
+    };
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        disposables.clear();
-    }
+
+
 }
