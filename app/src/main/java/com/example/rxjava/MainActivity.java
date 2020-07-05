48ccdd6d3ab9bfc5425a03c5c072b0bb65728dc8
Vasile <vaseoa@yahoo.com>
2020-07-05 11:11:33 +0300

    first

--- app/src/main/java/com/example/rxjava/MainActivity.java
+++ app/src/main/java/com/example/rxjava/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.rxjava;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
 public class MainActivity extends AppCompatActivity {
+    CompositeDisposable disposables = new CompositeDisposable();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Observable<Task> taskObservable = Observable // create a new Observable object
+                .fromIterable(Task.createTasksList()) // apply 'fromIterable' operator
+                .subscribeOn(Schedulers.io()) // designate worker thread (background)
+                .observeOn(AndroidSchedulers.mainThread()); // designate observer thread (main thread)
+
+
+
+        taskObservable.subscribe(new Observer<Task>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                disposables.add(d);
+            }
+            @Override
+            public void onNext(Task task) { // run on main thread
+
+                Log.d("vasea", "onNext: : " + task.getDescription());
+            }
+            @Override
+            public void onError(Throwable e) {
+
+            }
+            @Override
+            public void onComplete() {
+                Log.d("vasea", "onComplete: : ");
+            }
+        });
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        disposables.clear();
     }
 }
