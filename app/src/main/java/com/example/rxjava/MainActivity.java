d5af93118cb00d06e9adedf63e7b2c4287dfbb39
Vasile <vaseoa@yahoo.com>
2020-07-05 19:18:56 +0300

    first

--- app/src/main/java/com/example/rxjava/MainActivity.java
+++ app/src/main/java/com/example/rxjava/MainActivity.java
@@ -2,60 +2,103 @@ package com.example.rxjava;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.SearchView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.concurrent.TimeUnit;
+
 import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
-import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
+    //ui
+    private SearchView searchView;
+
+    // vars
+    private CompositeDisposable disposables = new CompositeDisposable();
+    private long timeSinceLastRequest; // for log printouts only. Not part of logic.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        searchView = findViewById(R.id.search);
 
-        Observable.fromIterable(Task.createTasksList())
-                .map(function)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<String>() {
+        timeSinceLastRequest = System.currentTimeMillis();
+
+        // create the Observable
+        Observable<String> observableQueryText = Observable
+                .create(new ObservableOnSubscribe<String>() {
                     @Override
-                    public void onSubscribe(Disposable d) {
+                    public void subscribe(final ObservableEmitter<String> emitter) throws Exception {
+
+                        // Listen for text input into the SearchView
+                        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                            @Override
+                            public boolean onQueryTextSubmit(String query) {
+                                return false;
+                            }
+
+                            @Override
+                            public boolean onQueryTextChange(final String newText) {
+                                if (!emitter.isDisposed()) {
+                                    emitter.onNext(newText); // Pass the query to the emitter
+                                }
+                                return false;
+                            }
+                        });
+
+
 
                     }
+                })
+                .debounce(500, TimeUnit.MILLISECONDS) // Apply Debounce() operator to limit requests
+                .subscribeOn(Schedulers.io());
 
-                    @Override
-                    public void onNext(String task) {
-                        Log.d("vasea", "onNext: " + task);
-                    }
+        // Subscribe an Observer
+        observableQueryText.subscribe(new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                disposables.add(d);
+            }
 
-                    @Override
-                    public void onError(Throwable e) {
+            @Override
+            public void onNext(String s) {
+                Log.d(TAG, "onNext: time  since last request: " + (System.currentTimeMillis() - timeSinceLastRequest));
+                Log.d(TAG, "onNext: search query: " + s);
+                timeSinceLastRequest = System.currentTimeMillis();
 
-                    }
+                // method for sending a request to the server
+                sendRequestToServer(s);
+            }
 
-                    @Override
-                    public void onComplete() {
+            @Override
+            public void onError(Throwable e) {
+            }
 
-                    }
-                });
-
-
+            @Override
+            public void onComplete() {
+            }
+        });
     }
 
-    Function<Task, String> function = new Function<Task, String>() {
-        @Override
-        public String apply(Task task) {
-            return task.toString();
-        }
-    };
+    // Fake method for sending a request to the server
+    private void sendRequestToServer(String query) {
+        Log.d(TAG, "sendRequestToServer: " + query);
+    }
 
-
-
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        disposables.clear(); // clear disposables
+    }
 }
