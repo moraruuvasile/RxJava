72a1c63b8327c0df04fbf03adf4e04411ba5e5f8
Vasile <vaseoa@yahoo.com>
2020-07-06 08:34:20 +0300

    first

--- app/src/main/java/com/example/rxjava/Post.java
+++ app/src/main/java/com/example/rxjava/Post.java
@@ -3,8 +3,6 @@ package com.example.rxjava;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-import org.w3c.dom.Comment;
-
 import java.util.List;
 
 public class Post {
